6e75c0d1deb92d84f002f4390e544ff03a6a80bd
Greg Sneed Admin <gsadmin@ad.sneedlabs.com>
2021-05-08 22:28:27 -0500

    WIP

--- PoshTube.ps1
+++ PoshTube.ps1
@@ -11,3 +11,22 @@ Function Get-YoutubeCode
     $response = Invoke-RestMethod $RequestURI -Method 'POST' -Headers $headers
     Return $response 
 }
+
+Function Step2 
+{
+    <#
+    Gets the initial device request code from Google's API.
+    #>
+    Param (
+        $ClientID,
+        $ClientSecret,
+        $DeviceCode,
+        $GrantType = "http://oauth.net/grant_type/device/1.0"
+        
+    )
+
+    $URI = "https://accounts.google.com/o/oauth2/token?client_id={0}&client_secret={1}&code={2}&grant_type={3}" -f $ClientID, $ClientSecret, $DeviceCode, $GrantType
+    $response1 = Invoke-RestMethod $URI -Method 'POST' -Headers $headers
+    $response1 | ConvertTo-Json
+}
+ 
