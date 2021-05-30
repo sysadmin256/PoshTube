f12ca33db265fcde3c2dae4a848779829a0e7a45
Greg Sneed Admin <gsadmin@ad.sneedlabs.com>
2021-05-29 21:55:33 -0500

    Updates

--- PoshTube.ps1
+++ PoshTube.ps1
@@ -24,9 +24,40 @@ Function Step2
         $GrantType = "http://oauth.net/grant_type/device/1.0"
         
     )
+    <#
+    # May need logic here for refresh token.  Check response and supply refresh token if it's a valid option.
+    Open browser, display code to user
+    Loop here until the user authenticates
+    Unauthenticated return: 
+        PS C:\Admin\Repo\PoshTube> $DeviceCode
+        {
+            "error":  "authorization_pending",
+            "error_description":  "Precondition Required"
+        }
+    Authenticated return: 
+        PS C:\Admin\Repo\PoshTube> $DeviceCode
+        {
+            "access_token":  "ya29.a0AfH6SMCk69b4vjpZI1p7uDEfnqKMaP1Npxd8JD9Tv-CnwkxdT58p9n1mH612wnfxqAy2CZcNO3VArr4qPokt9HeLpbqwCCPiSLG9A4YSO4F089K8UzS9neTrjlTjgtKHbKD2AQyeeGQCRAR62ZtzA-BEzRhC",
+            "expires_in":  3599,
+            "refresh_token":  "1//04XQEEhlf98chCgYIARAAGAQSNwF-L9Irsw1ADS1RnM9uXIF8TAS7OO9cs55XjqoZAP8Z3UwHzc9sn7oNlRPH_o1WJpDi_QSZHUc",
+            "scope":  "https://www.googleapis.com/auth/youtube",
+            "token_type":  "Bearer"
+        }
+    Save the access token and the refresh token for later use
+    #>
 
+    #This code checks the status shown above
     $URI = "https://accounts.google.com/o/oauth2/token?client_id={0}&client_secret={1}&code={2}&grant_type={3}" -f $ClientID, $ClientSecret, $DeviceCode, $GrantType
     $response1 = Invoke-RestMethod $URI -Method 'POST' -Headers $headers
     $response1 | ConvertTo-Json
 }
+
+
+#Testing
+$RequestCode = Get-YoutubeCode -ClientID "265341207630-7mqm6fpa9u3fvq5tucf9i105o33v3sbs.apps.googleusercontent.com"
+# Start Request for device
+$DeviceCode = step2 -ClientID "265341207630-7mqm6fpa9u3fvq5tucf9i105o33v3sbs.apps.googleusercontent.com" `
+      -ClientSecret "Z14UzYv5gIfJmRv890bVFm_w" `
+      -DeviceCode $RequestCode.device_code
+
  
---
+++ Refrence Script.ps1
@@ -0,0 +1,30 @@
+# Get access token for app
+$response = Invoke-RestMethod 'https://accounts.google.com/o/oauth2/device/code?client_id=265341207630-7mqm6fpa9u3fvq5tucf9i105o33v3sbs.apps.googleusercontent.com&scope=https://www.googleapis.com/auth/youtube' -Method 'POST' -Headers $headers
+$response #| ConvertTo-Json
+
+#Auth prompt for user
+$URI = "https://accounts.google.com/o/oauth2/token?client_id=265341207630-7mqm6fpa9u3fvq5tucf9i105o33v3sbs.apps.googleusercontent.com&client_secret=Z14UzYv5gIfJmRv890bVFm_w&code={0}&grant_type=http://oauth.net/grant_type/device/1.0" -f $Response.device_code
+$response1 = Invoke-RestMethod $URI -Method 'POST' -Headers $headers
+$response1 | ConvertTo-Json
+
+# Add prompt here to open URL and have user authenticate
+
+
+# Make actual API request to schedule stream
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$AccessToken = "Bearer {0}" -f $response1.access_token
+$headers.Add("Authorization", $AccessToken)
+$headers.Add("Content-Type", "text/plain")
+
+$body = "{`"snippet`":{`"scheduledStartTime`":`"2021-05-08T18:00:00`",`"title`":`"This is a test`"},`"status`":{`"privacyStatus`":`"private`"}}"
+
+$response3 = Invoke-RestMethod 'https://youtube.googleapis.com/youtube/v3/liveBroadcasts?part=snippet&part=contentDetails&part=status' -Method 'POST' -Headers $headers -Body $body
+$response3 | ConvertTo-Json
+
+#The API returns an HTTP 401 response code (Unauthorized) if you submit a request to access a protected resource with an access token that is expired, bogus, improperly scoped, or invalid for some other reason.
+
+
+#Refresh Token
+$RefreshToken = $Response1.refresh_token
+$response = Invoke-RestMethod "https://accounts.google.com/o/oauth2/token?client_id=265341207630-7mqm6fpa9u3fvq5tucf9i105o33v3sbs.apps.googleusercontent.com&client_secret=Z14UzYv5gIfJmRv890bVFm_w&refresh_token=$RefreshToken&grant_type=refresh_token" -Method 'POST' -Headers $headers
+$response | ConvertTo-Json
